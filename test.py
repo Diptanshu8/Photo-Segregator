5898cf5b02e9eb75bb9592cf2997eda8b1e9bdf8
Diptanshu8@mac <diptanshuj@gmail.com>
2018-05-28 11:55:02 +0530

    Made changes to highlight a single face at a time.
    FW: save the identified face samples and take user inputs for whose face
    is it. For now stick to input from command line. Later we will resolve
    how to make a UI for it.

--- test.py
+++ test.py
@@ -1,20 +1,7 @@
 import face_recognition
 import cv2
 import os,sys
-def print_usage():
-    print "Usage:\tpython test.py <directory of test images or a single test_image>"
-
-def check_params():
-    if len(sys.argv) < 2:
-        print_usage()
-        exit()
-    if os.path.isdir(sys.argv[1]):
-        test_dir = sys.argv[1]
-        test_image = [os.path.join(test_dir,item) for item in os.listdir("test_images") if item.endswith(".jpg")]
-    else:
-        test_image = [sys.argv[1]]
-    return test_image 
-
+import param_process_functions as pp
 def show_image(image):
     cv2.imshow('Image',image)
     while(1):
@@ -24,11 +11,30 @@ def show_image(image):
 	elif k==32:
 	    break
 
+def show_all_faces(face_locations,img):
+    for (top, right, bottom, left) in face_locations:
+        # Draw a box around the face
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+        #roi_gray = gray[y:y+h, x:x+w]
+        #roi_color = img[y:y+h, x:x+w]
+    return img
+
+def show_individual_faces(face_locations,img):
+    o_img = img.copy()
+    for (top, right, bottom, left) in face_locations:
+        cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+        #t_sample = img[y: y + h, x: x + w]
+        yield(img)
+        #yield (t_sample,img)
+        img = o_img.copy()
+
+
 def main():
-    test_image = check_params()
+    test_image = pp.check_params()
     print "Done compiling params and total number of images to be processed is: " + str(len(test_image))
     for test in test_image:
     	print "Processing image:\t"+test
+
 	# The inbuilt load_image_file function messes up with the color scale of the image.
 	# Since face detection is done on gray image, the original image is read using opencv.
 	# In the built in function, PIL is used to read the image.
@@ -36,16 +42,19 @@ def main():
 
         img = cv2.imread(test)
         face_locations = face_recognition.face_locations(img)
-	show_image(img)
-	print "Drawing rectangles around detected faces if any."
-	face_count = 0
-	for (top, right, bottom, left) in face_locations:
-	    # Draw a box around the face
-	    cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-	    face_count+=1
-	print "Drew {} faces.".format(face_count)
-	show_image(img)
-	cv2.destroyAllWindows()
+        print "Detected {} faces in this image.".format(len(face_locations))
+        
+        if len(face_locations)>0:
+            show_image(img)
+            # uncomment the following line to mark all the faces.
+            #img = show_all_faces(face_locations,img) 
+            #show_image(img)
+
+            # Generator implementation for individual faces
+            for i in show_individual_faces(face_locations, img):
+                #images.append(sample)
+                show_image(i)
+            cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
