77fe1f8163f46dc1c45cdfd611bd9a1dad949a45
Diptanshu8@mac <diptanshuj@gmail.com>
2018-05-30 10:38:59 +0530

    Added argparse support to train utility and option to delete the trained cache.

--- train_utility.py
+++ train_utility.py
@@ -1,8 +1,9 @@
 import cv2
 import os
+import shutil
 import config
+import argparse
 import image_handlers as ih
-
 
 def show_train_snippets():
     snips = [os.path.join(config.train_cache,item) for item in os.listdir(config.train_cache)]
@@ -10,6 +11,37 @@ def show_train_snippets():
         img = cv2.imread(snip)
         print "Image: {}".format(snip)
         ih.show_image(img)
+def delete_train_snippets():
+    user_choice = raw_input("Are you sure you want to delete the trained snippets?")
+    valid = {"yes": True, "y": True, "Y": True, "YES" : True, "Yes": True,
+            "no": False, "n": False, "NO": False, "No": False, "N": False}
+    if not valid.has_key(user_choice):
+        print "Valid user_choices are:"
+        print valid.keys()
+        delete_train_snippets()
+    else:
+        if valid[user_choice]:
+            try:
+                shutil.rmtree(config.train_cache)
+                print "Removed the trained snippets that were cached."
+            except Exception as e:
+                print e
+        else:
+            print "Exiting without removing the trained cache."
 
 if __name__=="__main__":
-    show_train_snippets()
+    parser = argparse.ArgumentParser(description='This is a Photo Segregator Application.')
+    group = parser.add_argument_group('utility')
+    group.add_argument('-s', action="store_true",
+            help="Pass this argument to see all the saved snippets identified by the user.")
+
+    group.add_argument('-d', action="store_true",
+            help="Pass this argument to delete all the saved snippets identified by the user.")
+    
+    results = parser.parse_args()
+    if not any(vars(results).values()):
+        parser.error("No arguments provided. Either of -s or -d to be provided.")
+    elif results.s:
+        show_train_snippets()
+    elif results.d:
+        delete_train_snippets()
