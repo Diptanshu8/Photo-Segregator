e46a8277ee622a03b79b3869a250cbf9e1d9a0e0
Diptanshu8@mac <diptanshuj@gmail.com>
2018-05-30 10:49:01 +0530

    Added delete train cache function and made sure that all the file unavailbility errors are managed.

--- train_utility.py
+++ train_utility.py
@@ -6,11 +6,15 @@ import argparse
 import image_handlers as ih
 
 def show_train_snippets():
+    if not os.path.isdir(config.train_cache) or not os.listdir(config.train_cache):
+        print "Error: There are no trainned snippets as cache."
+        return 
     snips = [os.path.join(config.train_cache,item) for item in os.listdir(config.train_cache)]
-    for snip in snips:
+    for i,snip in enumerate(snips):
         img = cv2.imread(snip)
-        print "Image: {}".format(snip)
+        print "Image<{}>: {}".format(i,snip)
         ih.show_image(img)
+
 def delete_train_snippets():
     user_choice = raw_input("Are you sure you want to delete the trained snippets?")
     valid = {"yes": True, "y": True, "Y": True, "YES" : True, "Yes": True,
