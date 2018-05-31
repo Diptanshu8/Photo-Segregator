dfc9e2bed0f9741868b34a5a8306b5dbfd288b53
Diptanshu8@mac <diptanshuj@gmail.com>
2018-05-31 17:30:11 +0530

    Seperated the function to check for train_chache and another function returning the list of files found in train_chache. This list is then used to generate and load known encodings.

--- recognize_utility.py
+++ recognize_utility.py
@@ -5,9 +5,12 @@ def check_train_snippets():
     if not os.path.isdir(config.train_cache) or not os.listdir(config.train_cache):
         print "Error: There are no trainned snippets as cache."
         return False 
-    snips = [item for item in os.listdir(config.train_cache)]
-    print "The following are the train snippets in random order.\nTo view them simply run train_utility.py with proper parameters."
-    for i,snip in enumerate(snips):
-        print "Image<{}>: {}".format(i,snip)
     return True
 
+def print_train_snippets():
+    if check_train_snippets():
+        snips = [item for item in os.listdir(config.train_cache)]
+        print "The following are the train snippets in random order.\nTo view them simply run train_utility.py with proper parameters."
+        for i,snip in enumerate(snips):
+            print "Image<{}>: {}".format(i+1,snip)
+        return snips
