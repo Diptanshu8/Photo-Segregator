345d67358b0a635e11a8fa20094b6d805159a536
Diptanshu8@mac <diptanshuj@gmail.com>
2018-05-31 17:28:43 +0530

    Made variable changes to maintain uniformity of the code.

--- train.py
+++ train.py
@@ -13,23 +13,24 @@ def clear_cache():
         pass
 
 def main():
-    test_image = pp.check_params()
+    train_images = pp.check_params()
     print "Done compiling params."
-    print "Total number of images to be processed is: " + str(len(test_image))
-    for test in test_image:
-    	print "Processing image:\t"+test
+    print "Total number of images to be processed is: " + str(len(train_images))
+    for sample in train_images:
+    	print "Processing image:\t"+sample
 
 	# The inbuilt load_image_file function messes up with the color scale of the image.
 	# Since face detection is done on gray image, the original image is read using opencv.
 	# In the built in function, PIL is used to read the image.
-        #image = face_recognition.load_image_file(test)
+        #image = face_recognition.load_image_file(sample)
 
-        img = cv2.imread(test)
+        img = cv2.imread(sample)
         face_locations = face_recognition.face_locations(img)
         
         if len(face_locations)>0:
             print "Detected {} faces in this image.".format(len(face_locations))
             #ih.show_image(img)
+
             # uncomment the following line to mark all the faces.
             #img = ih.show_all_faces(face_locations,img) 
             #show_image(img)
@@ -37,9 +38,8 @@ def main():
             # Generator implementation for individual faces
             for face in ih.show_individual_faces(face_locations, img):
                 ih.show_face_snippet()
-                #ih.show_face_snippet(face_snippet)
-                #ih.show_image(face_snip)       #FW: Save this snippet as a temporary test data.
-                #show_image(faces)
+                #show_image(face)      # Uncomment this line to simulatenously see the highlighted face 
+                                        # in original image.
             cv2.destroyAllWindows()
         else:
             print "No face detected in this image."
