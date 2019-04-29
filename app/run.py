7b7625465437d596582f9de372e7cb7615f43654
Diptanshu8 <diptanshuj@gmail.com>
2019-04-29 19:08:36 +0530

    Add support to serve images from static folder.

--- app/run.py
+++ app/run.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, url_for
 import os
+from flask_images import Images
 
 
 app = Flask(__name__)
+app.secret_key = "temp"
+images = Images(app)
+
+def get_images_from_static():
+	return os.listdir("static")
 @app.route('/')
 def index():
-    return render_template("base.html")
+	images=get_images_from_static()
+	return render_template("base.html",images= images)
 app.run(host="0.0.0.0")
