b239a54b6b3224896afe5c52d72dd53a595e5a7f
Diptanshu8 <diptanshuj@gmail.com>
2019-09-29 20:44:28 +0530

    ng-click not working. onclick event is triggered for local function from html but not via included js. temp commit to debug this issue.
    Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/run.py
+++ app/run.py
@@ -8,22 +8,20 @@ app.secret_key = "temp"
 #images = Images(app)
 
 def get_images_from_static():
-	images = [item for item in os.listdir("static") if ".jpg" in item]
-	return images
+        images = [item for item in os.listdir("static") if ".jpg" in item]
+        return images
 
 @app.route('/')
 def home_page():
-	images=get_images_from_static()
-	return render_template("base.html",images= images)
+        images=get_images_from_static()
+        return render_template("base.html",images= images)
 
-@app.route('/scan_image', methods=['POST','GET'])
+@app.route('/test', methods=['POST','GET'])
 def scan_image():
-	print("Inside scan_image")
-	print(request)
-	print(request.data.decode())
-	# Get image name from the result by searching for .jpg ext as of now. Extend to other extensions too.
-	# Process image and add the processed image with squares to the image list and render
-	images=get_images_from_static()
-	return render_template("base.html",images= images)
+        # Get image name from the result by searching for .jpg ext as of now. Extend to other extensions too.
+        # Process image and add the processed image with squares to the image list and render
+	return
+        #images=get_images_from_static()
+        #return render_template("base.html",images= images)
 
-app.run(host="0.0.0.0", debug=True)
+app.run(debug=True)
--- app/static/main.js
+++ app/static/main.js
@@ -1,29 +1,20 @@
 (function () {
 
-  'use strict';
-
   angular.module('PhotoSegregatorApp', [])
 
-  .controller('PhotoScanController', ['$scope', '$log', '$http', '$timeout',
-    function($scope, $log, $http, $timeout) {
-	$scope.img = ""
-    $scope.scanImage = function(value) {
+  .controller('PhotoScanController', ['$scope', '$log',
+    function($scope, $log) {
 
-      $log.log("Inside scanImage");
-
-      // get the URL from the input
-      var userInput = $scope.img;
-
-      // fire the API request
-      $http.post('/scan_image', {"img": userInput}).
-        success(function(results) {
-          $log.log(value);
-          $log.log(userInput);
-        }).
-        error(function(error) {
-          $log.log(error);
-        });
+    $scope.test = function() {
+        alert("INside JS function");
+            //$.each($("input:checked"), function(){            
+                        //    $scope.selected.push($(this).id());
+            //            alert("My checked images are: " + $(this.id()));
+             //           });
     };
+
+	$scope.test_image = function() {
+		alert("SKRILL hit image callback");
   }
   ]);
 }());
