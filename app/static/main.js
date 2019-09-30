459ad9c9f655adc0a6cd2e29ec6e1ee004af5394
Diptanshu8 <diptanshuj@gmail.com>
2019-09-30 23:23:50 +0530

    ng-click for Scan button is logging the list of selected images properly. Also, add test_images to .gitignore.
    Signed-off-by: Diptanshu8 <diptanshuj@gmail.com>

--- app/static/main.js
+++ app/static/main.js
@@ -4,18 +4,16 @@
 
   .controller('PhotoScanController', ['$scope', '$log',
     function($scope, $log) {
+    $scope.selected = [];
 
-    $scope.test = function() {
-        alert("INside JS function");
-            //$.each($("input:checked"), function(){            
-                        //    $scope.selected.push($(this).id());
-            //            alert("My checked images are: " + $(this.id()));
-             //           });
-    };
-
-	$scope.test_image = function() {
-		alert("SKRILL hit image callback");
-  }
+    $scope.scanImages = function() {
+        $.each($("input[name='cb']:checked"), function(){            
+                            $scope.selected.push($(this).attr('id'));
+                        });
+        $log.log("My checked images are: " + $scope.selected.join(", "));
+        $scope.selected = [];
+        };
+    }
   ]);
 }());
 
